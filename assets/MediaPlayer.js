1ff6913918f4ed01edf43d6fc1b45e06b6e3f4a4
Vicente Benavides <vicente.benavides0@gmail.com>
2020-09-07 17:26:35 -0500

    actualizacion del proyecto

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,43 @@
+function MediaPlayer(config) {
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+};
+
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugins => {
+    plugins.run(this);
+  })
+}
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused){
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+}
+
+MediaPlayer.prototype.toggleMute = function() {
+  this.media.muted
+  ? this.unmute()
+  : this.mute();
+};
+
+export default MediaPlayer;
